983bc09daab2097b34b0f68f18e4f95caa869894
neiron94 <71330235+neiron94@users.noreply.github.com>
2022-12-09 15:52:13 +0100

    Update basefilter.py

--- spam_filter/basefilter.py
+++ spam_filter/basefilter.py
@@ -18,7 +18,6 @@ class BaseFilter:
     def train(self, path):
         '''
         Function to filter learning
-
         :param path:    path to folder with emails (string)
         '''
         pass
@@ -27,27 +26,24 @@ class BaseFilter:
     def test(self, path):
         '''
         Function to write result of predictions
-
         :param path:    path to folder with emails (string)
         '''
         OUTP = "!prediction.txt"  # Filename to write result of prediction
 
         corpus = Corpus(path)   # Filter's corpus
         for file in corpus.emails():   # Make predictions
-            self.predict(path, file[0], file[1])
+            self.predict(path, file[0])
 
         # Write predictions to file
         prediction_path = os.path.join(path, OUTP)
         utils.write_classification_to_file(prediction_path, self.predictions)
 
     
-    def predict(self, path, filename, content):
+    def predict(self, path, filename):
         '''
         Function to make prediction
         Side effects: Fill self.predictions with {filename:prediction}
-
         :param path:        path to directory with tested emails (string)
         :param filename:    name of email (string)
-        :param content:     email content (string)
         '''
         pass
